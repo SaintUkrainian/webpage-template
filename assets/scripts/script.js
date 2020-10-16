1f50ae6bc1099194722b97f379d9a49ef83740ba
Denys Matsenko <idanchik47@gmail.com>
2020-10-16 21:45:44 +0300

    slider created

---
+++ assets/scripts/script.js
@@ -0,0 +1,107 @@
+var slideIndex = 0;
+var currentSlideIndex = 0;
+var slideArray = [];
+
+function Slide(title, subtitle, background, link) {
+    this.title = title;
+    this.subtitle = subtitle;
+    this.background = background;
+    this.link = link;
+    this.id = "slide" + slideIndex;
+    slideIndex++;
+    slideArray.push(this);
+}
+
+// ви можете зробити скільки завгодно слайдів
+
+var slide1 = new Slide(
+    "React.js",
+    "React",
+    "./assets/images/reactmini.png",
+    "https://reactjs.org/"
+);
+
+var slide2 = new Slide(
+    "Angular.js",
+    "Angular",
+    "./assets/images/angular.jpg",
+    "https://angular.io/"
+);
+
+var slide3 = new Slide(
+    "Vue.js",
+    "Vue",
+    "./assets/images/vue.jpg",
+    "https://vuejs.org/"
+);
+
+function buildSlider() {
+    var myHTML;
+    for (var i = 0; i < slideArray.length; i++) {
+        myHTML +=
+            "<div id='" +
+            slideArray[i].id +
+            "' class='singleSlide' style='background-image:url(" +
+            slideArray[i].background +
+            ");'>" +
+            "<div class='slideOverlay'>" +
+            "<h1 id=>" +
+            slideArray[i].title +
+            "</h1>" +
+            "<h4>" +
+            slideArray[i].subtitle +
+            "</h4>" +
+            "<a class='slider' href='" +
+            slideArray[i].link +
+            "' target='_blank'>Open Link</a>" +
+            "</div>" +
+            "</div>";
+    }
+
+    document.getElementById("mySlider").innerHTML = myHTML;
+    document.getElementById("slide" + currentSlideIndex).style.left = 0;
+}
+
+buildSlider();
+
+function prevSlide() {
+    var nextSlideIndex;
+    if (currentSlideIndex === 0) {
+        nextSlideIndex = slideArray.length - 1;
+    } else {
+        nextSlideIndex = currentSlideIndex - 1;
+    }
+
+    document.getElementById("slide" + nextSlideIndex).style.left = "-100%";
+    document.getElementById("slide" + currentSlideIndex).style.left = 0;
+
+    document
+        .getElementById("slide" + nextSlideIndex)
+        .setAttribute("class", "singleSlide slideInLeft");
+    document
+        .getElementById("slide" + currentSlideIndex)
+        .setAttribute("class", "singleSlide slideOutRight");
+
+    currentSlideIndex = nextSlideIndex;
+}
+
+function nextSlide() {
+    var nextSlideIndex;
+    if (currentSlideIndex === slideArray.length - 1) {
+        nextSlideIndex = 0;
+    } else {
+        nextSlideIndex = currentSlideIndex + 1;
+    }
+
+    document.getElementById("slide" + nextSlideIndex).style.left = "100%";
+    document.getElementById("slide" + currentSlideIndex).style.left = 0;
+
+    document
+        .getElementById("slide" + nextSlideIndex)
+        .setAttribute("class", "singleSlide slideInRight");
+    document
+        .getElementById("slide" + currentSlideIndex)
+        .setAttribute("class", "singleSlide slideOutLeft");
+
+    currentSlideIndex = nextSlideIndex;
+}
